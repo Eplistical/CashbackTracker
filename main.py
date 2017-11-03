65cf6fb658f86480df3176d33269427419829300
Eplistical <gaohan@sas.upenn.edu>
2017-11-03 11:22:42 -0400

    add argment parsing in main

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python3
-
-import os
+import argparse
+import time
+# local modules
 from ebates_tracker import *
 
 
+def parsearg():
+    """parse arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('runcount',
+            type=int,
+            help="total times to run")
+
+    parser.add_argument('-i','--interval',
+            type=float,
+            default=1.0,
+            help="interval between two run, unit: hour, default: 1.0")
+
+
+    return parser.parse_args()
+
 def main():
     tracker = EbatesTracker()
-    tracker.process(save_history=True)
+    args = parsearg()
+
+    # check args
+    if args.runcount <= 0 or args.interval <= 0:
+        raise ParseError('runcount and interval must be positive')
+
+    # loop for args.runcount times
+    for _ in range(args.runcount):
+        tracker.process(save_history=True)
+        if _ < args.runcount - 1:
+            time.sleep(args.interval * 3600)
 
 
 if __name__ == '__main__':
