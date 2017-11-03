9b14c949a681b67116dd485a54b0073a2b0b7ba5
Eplistical <gaohan@sas.upenn.edu>
2017-11-03 11:28:55 -0400

    reformatted

--- main.py
+++ main.py
@@ -12,27 +12,22 @@ def parsearg():
     parser.add_argument('runcount',
             type=int,
             help="total times to run")
-
     parser.add_argument('-i','--interval',
             type=float,
             default=1.0,
             help="interval between two run, unit: hour, default: 1.0")
-
-
     return parser.parse_args()
 
 def main():
     tracker = EbatesTracker()
     args = parsearg()
-
     # check args
     if args.runcount <= 0 or args.interval <= 0:
         raise ParseError('runcount and interval must be positive')
-
     # loop for args.runcount times
-    for _ in range(args.runcount):
+    for i in range(args.runcount):
         tracker.process(save_history=True)
-        if _ < args.runcount - 1:
+        if i < args.runcount - 1:
             time.sleep(args.interval * 3600)
 
 
