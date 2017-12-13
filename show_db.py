83c1a6f4794376554c065f206881304fbaab321e
gaohan <gaohanm@gmail.com>
2017-12-13 11:59:54 -0500

    save

--- show_db.py
+++ show_db.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from ebates_tracker import *
-from cashback_tracker import CashbackTrackerDBMgr
+from trackers.cashback_tracker import CashbackTrackerDBMgr
 
 def show_db():
     dbmgr = CashbackTrackerDBMgr()
-    for record in dbmgr.show(name="Macy's", source="Ebates"):
+    for record in dbmgr.show():
         for tmp in record:
             print('%30s' % tmp, end=' ')
         print()
